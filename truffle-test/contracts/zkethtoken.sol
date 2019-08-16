e966b40efffa878103738a91d242b45845511d11
trustfarm-vr1 <trustfarm.info@gmail.com>
2019-08-16 22:50:38 +0900

    updates contract bug
    https://rinkeby.etherscan.io/token/0xf201ced3f4729d31d83411a6984c7afdcfe8061a

--- truffle-test/contracts/zkethtoken.sol
+++ truffle-test/contracts/zkethtoken.sol
@@ -3,12 +3,17 @@ import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
  
 contract zkethtoken is StandardToken {
     string public name = "ZKETH1"; 
-    string public symbol = "ZKETH"; //Currency Symbol
-    uint public decimals = 18; // 1ETH is start from 18digit 
-    uint public INITIAL_SUPPLY = 21000000 * (10 ** decimals); 
+    string public symbol = "ZKETH1"; //Currency Symbol
+    uint public decimals = 18; // 1ETH is start from 18digit (소수점 자리수) 
+    uint public totalSupply  = 21000000 * (10 ** decimals); 
+    string public version = 'v0.1';
  
+    /* 아래는 ICO Edition */
+    uint256 public unitsOneEthCanBuy;     // 1ETH 당 토큰 배분비율
+    uint256 public totalEthInWei;         // ICO ETH SoftCap .  
+    address public fundsWallet;           // ICO ETH 모금지갑 .
     //construct
     function zkethtoken() public {
-        balances[msg.sender] = INITIAL_SUPPLY;
+        balances[msg.sender] = totalSupply;
     }
 }
